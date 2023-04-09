207b2c3e3fe49b0701663966e6752639760d9be9
taicedtea <54014240+taicedtea@users.noreply.github.com>
2023-04-09 11:31:05 -0400

    Added page placeholders, added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Navigation from './Navigation';
+
 const Header = () => {
     return (
-      <div className="item">
+      <div>
+        <Navigation />
       </div>
     );
   };
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,28 @@
 import React from 'react';
 const Navigation = () => {
     return (
-      <div className="item">
-      </div>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark ps-4">
+        <a class="navbar-brand" href="#">Tai Disla</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">About Me</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Portfolio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Resume</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   };
   
